32fe2ef47799f068f328c113bc14e9edcbe07df2
Erick Pac <erickj.pac@gmail.com>
2025-05-20 08:27:53 -0600

    refactor(resend): replace EmailSenderService with ResendService as the email sender implementation and streamline module exports

--- src/modules/resend/infrastructure/services/resend.service.ts
+++ src/modules/resend/infrastructure/services/resend.service.ts
@@ -2,26 +2,27 @@ import { Injectable, Logger } from '@nestjs/common';
 import { Resend } from 'resend';
 import { IEmailParams } from '../../domain/interfaces/email-params.interface';
 import { env } from '../../../../config/env.config';
+import { IEmailSender } from '../../domain/interfaces/email-sender.interface';
 
 @Injectable()
-export class ResendService {
+export class ResendService implements IEmailSender {
   private readonly logger = new Logger(ResendService.name);
   private readonly resend: Resend;
+  private readonly defaultFrom: string;
 
   constructor() {
     this.resend = new Resend(env.RESEND_API_KEY);
+    this.defaultFrom = env.RESEND_FROM_EMAIL || 'no-reply@leftovers.app';
   }
 
-  async sendEmail(params: IEmailParams) {
-    const defaultFrom = env.RESEND_FROM_EMAIL || 'no-reply@leftovers.app';
-
+  async sendEmail(params: IEmailParams): Promise<void> {
     try {
-      return this.resend.emails.send({
-        from: params.from || defaultFrom,
+      await this.resend.emails.send({
+        from: params.from || this.defaultFrom,
         to: params.to,
         subject: params.subject,
         html: params.html,
-        text: params.text,
+        text: params.text ?? '', // Ensure text is always a string
         replyTo: params.replyTo,
         cc: params.cc,
         bcc: params.bcc,
@@ -30,6 +31,8 @@ export class ResendService {
       this.logger.error(
         `Failed to send email to: ${Array.isArray(params.to) ? params.to.join(', ') : params.to}, Subject: ${params.subject}. Error: ${error}`,
       );
+
+      throw error; // Rethrow the error
     }
   }
 }
--- src/modules/resend/resend.module.ts
+++ src/modules/resend/resend.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ResendService } from './infrastructure/services/resend.service';
-import { EmailSenderService } from './infrastructure/services/email-sender.service';
 
 @Module({
   providers: [
-    ResendService,
     {
       provide: 'IEmailSender',
-      useClass: EmailSenderService,
+      useClass: ResendService,
     },
   ],
   exports: ['IEmailSender'],
