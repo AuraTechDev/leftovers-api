484b9ea416368d28ea33d5fa07d708f0ae37c80e
Erick Pac <erickj.pac@gmail.com>
2025-05-20 09:08:12 -0600

    Update Cloudinary service import path in test file for improved module structure
    - Changed the import path for CloudinaryService in the cloudinary.service.spec.ts test file to reflect the new directory structure under 'infrastructure/services'.
    - This adjustment enhances code clarity and maintains consistency with recent refactoring efforts in the Cloudinary module.
    
    These changes contribute to a more organized and maintainable codebase.

--- src/modules/cloudinary/infrastructure/__tests__/cloudinary.service.spec.ts
+++ src/modules/cloudinary/infrastructure/__tests__/cloudinary.service.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { CloudinaryService } from '../cloudinary.service';
+import { CloudinaryService } from '../services/cloudinary.service';
 
 // Mock cloudinary before import
 jest.mock('cloudinary', () => ({
